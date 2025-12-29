0198090a955e1ca94e76a5cc112d5ebdce897752
fadhel <fadhel@fadhel-X450JF>
2025-12-29 12:37:31 +0700

    Initial project

---
+++ src/test/java/com/delfa/springbootlogging/SpringbootloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delfa.springbootlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
